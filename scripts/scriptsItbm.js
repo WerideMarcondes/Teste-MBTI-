f04dfaf496af6b4fbd2dd2e14b61c8a70536dfa9
WerideMarcondes <weride.welide@hotmail.com>
2022-12-06 12:24:39 -0300

    Foram adionadas mais 3 Modais questionários

--- scripts/scriptsItbm.js
+++ scripts/scriptsItbm.js
@@ -2,24 +2,27 @@
 $(document).ready(function () {
   $('#myModal').modal('show');
 
-});
 
-//função chamar modal primeiro questionario
-function questionatio1() {
-  $("#quiz1").click();
-
-}
 
 //função fechar modal informaçoes iniciais
-$("#quiz1").click(function () {
+$("#cadastro").click(function () {
   $('#myModal').modal('hide');
 
 });
 
+});
 
-function cadastra() {
 
-  const input = document.querySelectorAll(".input")
+
+
+
+
+
+
+
+/*function cadastra() {
+
+  const input = document.querySelectorAll(".form-control")
 
   if (input.value != "") {
     Swal.fire(
@@ -36,3 +39,44 @@ function cadastra() {
 
   }
 }
+
+
+function resultado() {
+
+  /* variavies para de controle de impressão*/
+    let resultAaux1
+ 
+  
+  /* variavies para varrer os inputs*/
+    let EE = $("input[type=radio][value=E]:checked").length;
+    let II = $("input[type=radio][value=I]:checked").length;
+  
+  
+  
+    /* regras */
+    if (EE < II) {
+      resultAaux1 = "I"
+      Swal.fire(
+        'Perfil da cetgoria I continue responda o proximo questionario!',
+        '',
+        'success'
+      )
+    } else if (EE > II) {
+      resultAaux1 = "E"
+      Swal.fire(
+        'Perfil da cetgoria E responda o proximo questionario!',
+        '',
+        'success'
+      )
+    } else if (EE == II) {
+      resultAaux1 = "?"
+      Swal.fire(
+        'Você precisa determInar um lado predominante para esse questionário ',
+        '',
+        'error'
+      )
+    
+    }
+  
+   
+  
